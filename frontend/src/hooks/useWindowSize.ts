03c848d08b12b141eda896ad5e80418e2b7aeede
Dane Roelofs <dane.l.roelofs@gmail.com>
2023-06-06 00:04:14 -0700

    feat: adds hook

---
+++ frontend/src/hooks/useWindowSize.ts
@@ -0,0 +1,29 @@
+import React from "react";
+import { useState } from "react";
+
+interface WindowSize {
+  width: number;
+  height: number;
+}
+
+export const useWindowSize = (): WindowSize => {
+  const [windowSize, setWindowSize] = useState<WindowSize>({
+    width: 0,
+    height: 0,
+  });
+
+  React.useEffect(() => {
+    const handleResize = () =>
+      setWindowSize({ width: window.innerWidth, height: window.innerHeight });
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return windowSize;
+};

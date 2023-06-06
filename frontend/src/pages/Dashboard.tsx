75b7d46cfb66eb262a73672b97b5ac33841a8ee4
Dane Roelofs <dane.l.roelofs@gmail.com>
2023-06-06 00:08:58 -0700

    feat: :construction: starts dashboard layout

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,9 +1,48 @@
-import { Navbar } from "components/Navbar";
+import { faPlus, faUsers } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Card } from "components/Card";
+import React from "react";
+import { useAuthUser } from "react-auth-kit";
+import { Link } from "react-router-dom";
+import { clientRoutes } from "routes/clientRoutes";
+
+const AdminLayout = () => (
+  <div className="flex w-full justify-between">
+    <Link to={clientRoutes.admin.newUser}>
+      <Card className="flex w-56 cursor-pointer items-center justify-center">
+        <div className="flex items-center justify-center">
+          <h2>Create a New User</h2>
+          <FontAwesomeIcon icon={faPlus} className="pl-2" />
+        </div>
+      </Card>
+    </Link>
+    <Link to={clientRoutes.admin.allUsers}>
+      <Card className="flex w-56 cursor-pointer items-center justify-center">
+        <div className="flex w-56 items-center justify-center">
+          <h2>All Users</h2>
+          <FontAwesomeIcon icon={faUsers} className="pl-2" />
+        </div>
+      </Card>
+    </Link>
+  </div>
+);
 
 export const Dashboard = () => {
+  const auth = useAuthUser();
+
+  const isAdmin = React.useMemo(() => {
+    const authUserState = auth();
+
+    if (authUserState) {
+      return authUserState.isStaff || authUserState.isSuperuser;
+    }
+
+    return false;
+  }, [auth]);
+
   return (
-    <>
-      <Navbar />
-    </>
+    <div className="flex justify-between pt-10">
+      {isAdmin && <AdminLayout />}
+    </div>
   );
 };

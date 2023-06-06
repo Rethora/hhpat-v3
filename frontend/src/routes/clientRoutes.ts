6094765e280c6a6bdbeb4bac8b8b0c8421ea86f7
Dane Roelofs <dane.l.roelofs@gmail.com>
2023-06-06 00:06:48 -0700

    feat: :sparkles: adds routes

--- frontend/src/routes/clientRoutes.ts
+++ frontend/src/routes/clientRoutes.ts
@@ -1,12 +1,16 @@
 export const clientRoutes = {
   public: {
     root: "/",
-    signIn: "/sign-in",
+    signIn: "/sign-in/",
   },
   nonAdmin: {},
-  admin: {},
+  admin: {
+    users: "/users/",
+    allUsers: "/users/all/",
+    newUser: "/users/new/",
+  },
   authShared: {
-    dashboard: "/dashboard",
-    profile: "/profile",
+    dashboard: "/dashboard/",
+    profile: "/profile/",
   },
 };

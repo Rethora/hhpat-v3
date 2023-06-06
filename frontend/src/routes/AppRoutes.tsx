dbbfe7de676510d39dd72dd6b72dbf0de6bfb1e9
Dane Roelofs <dane.l.roelofs@gmail.com>
2023-06-06 00:09:31 -0700

    feat: :sparkles: adds app routes

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,15 +1,21 @@
 import {
   RouterProvider,
   createBrowserRouter,
+  defer,
   redirect,
 } from "react-router-dom";
-import { useIsAuthenticated } from "react-auth-kit";
+import { useAuthUser, useIsAuthenticated } from "react-auth-kit";
 import { SignIn } from "pages/SignIn";
 import { Dashboard } from "pages/Dashboard";
 import { clientRoutes } from "./clientRoutes";
-import { Root } from "pages/Root";
 import { AppContainer } from "components/AppContainer";
+import { NewUser } from "pages/NewUser";
+import { ShowUser } from "pages/ShowUser";
 import { useFetch } from "hooks/useFetch";
+import { apiRoutes } from "./apiRoutes";
+import { IUserResponse } from "types";
+import { RootLayout } from "layouts/RootLayout";
+import { AllUsers } from "pages/AllUsers";
 
 export const AppRoutes = () => {
   const isAuthenticated = useIsAuthenticated();
@@ -19,27 +25,69 @@ export const AppRoutes = () => {
   const router = createBrowserRouter([
     {
       path: clientRoutes.public.root,
-      element: <Root />,
-    },
-    {
-      path: clientRoutes.public.signIn,
-      element: <SignIn />,
-      loader: () => {
-        if (isAuthenticated()) {
-          return redirect(clientRoutes.authShared.dashboard);
-        }
-        return null;
-      },
-    },
-    {
-      path: clientRoutes.authShared.dashboard,
-      element: <Dashboard />,
-      loader: () => {
-        if (!isAuthenticated()) {
-          return redirect(clientRoutes.public.signIn);
-        }
-        return null;
-      },
+      element: <RootLayout />,
+      children: [
+        {
+          path: clientRoutes.public.signIn,
+          element: <SignIn />,
+          loader: () => {
+            if (isAuthenticated()) {
+              return redirect(clientRoutes.authShared.dashboard);
+            }
+            return null;
+          },
+        },
+        {
+          path: clientRoutes.authShared.dashboard,
+          element: <Dashboard />,
+          loader: () => {
+            if (!isAuthenticated()) {
+              return redirect(clientRoutes.public.signIn);
+            }
+            return null;
+          },
+        },
+        {
+          path: clientRoutes.admin.users,
+          loader: () => {
+            const authUser = auth();
+            if (!isAuthenticated() || !authUser) {
+              return redirect(clientRoutes.public.signIn);
+            } else if (authUser && !authUser.isStaff) {
+              return redirect(clientRoutes.authShared.dashboard);
+            }
+            return null;
+          },
+          children: [
+            {
+              path: "new",
+              element: <NewUser />,
+            },
+            {
+              path: "all",
+              element: <AllUsers />,
+              loader: async () => {
+                const usersPromise = fetchAuthenticated.get<IUserResponse[]>(
+                  apiRoutes.admin.users
+                );
+
+                return defer({ users: usersPromise });
+              },
+            },
+            {
+              path: ":userId",
+              element: <ShowUser />,
+              loader: async ({ params }) => {
+                const userPromise = fetchAuthenticated.get<IUserResponse>(
+                  `${apiRoutes.admin.users}${params.userId}/`
+                );
+
+                return defer({ user: userPromise });
+              },
+            },
+          ],
+        },
+      ],
     },
   ]);
 

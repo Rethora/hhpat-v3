7a3ebcb2f7cfc2ef9114ad61f3e2e5138b299267
Dane Roelofs <dane.l.roelofs@gmail.com>
2023-06-09 12:00:46 -0700

    refactor: :recycle: seperate entries from response

--- backend/backend/serializers.py
+++ backend/backend/serializers.py
@@ -1,24 +1,16 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
-from entry.serializers import EntrySerializer
-
 
 class UserSerializer(serializers.ModelSerializer):
-    entries = serializers.SerializerMethodField()
-
-    def get_entries(self, instance):
-        return EntrySerializer(instance.entry_set.all(), many=True).data
-
     class Meta:
         model = User
         fields = [
-            "pk",
+            "id",
             "email",
             "username",
             "first_name",
             "last_name",
             "is_staff",
             "is_superuser",
-            "entries",
         ]
